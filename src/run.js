222e427adeace784dccaf7676ca003f51522c63c
ordd <or.dd100@gmail.com>
2022-02-15 14:48:25 +0200

    added multiple wallet versions

--- src/run.js
+++ src/run.js
@@ -8,6 +8,8 @@ import fs from "fs";
 const nacl = TonWeb.utils.nacl;
 const DEFAULT_WALLET_VERSION = 'v3R2';
 
+const WALLET_VERSIONS = ['simpleR1', 'simpleR2', 'simpleR3', 'v2R1', 'v2R2', 'v3R1', 'v3R2', 'v4R1', 'v4R2'];
+
 function toHexString(byteArray) {
     return Array.prototype.map.call(byteArray, function (byte) {
         return ('0' + (byte & 0xFF).toString(16)).slice(-2);
@@ -21,7 +23,6 @@ async function privateKey2KeyPair(privateKey) {
 }
 
 async function mnemonic2PrivateKey(mnemonicWords) {
-    console.log(mnemonicWords);
     const keyPair = await tonMnemonic.mnemonicToKeyPair(mnemonicWords);
     const publicKeyHex = toHexString(keyPair.publicKey);
     console.log("PublicKey: " + "\n" + publicKeyHex + "\n");
@@ -33,25 +34,30 @@ async function mnemonic2PrivateKey(mnemonicWords) {
     return [publicKeyHex, privateKeyBase64, keyPair.publicKey];
 }
 
-async function getWalletAddress(publicKey, wc) {
+async function getWalletAddress(publicKey, wc, walletVersion) {
     const ton = new TonWeb();
     // const mainnetRpc = 'https://toncenter.com/api/v2/jsonRPC';
     // const ton = new TonWeb(new TonWeb.HttpProvider(mainnetRpc));
-    const WalletClass = ton.wallet.all[DEFAULT_WALLET_VERSION];
+    const WalletClass = ton.wallet.all[walletVersion];
     const walletContract = new WalletClass(ton.provider, {
         publicKey: publicKey,
         wc: wc
     });
     const address = (await walletContract.getAddress()).toString(true, true, true);
-    console.log("WalletAddress:" + "\n" + address);
+    console.log("Wallet Address " + walletVersion + ":\n" + address + "\n");
 }
 
 (async () => {
     const wc = process.argv[2]
-    const myMnemonicWords = process.argv.slice(3);
-    // if (!bip39.validateMnemonic(myMnemonicWords.join(" "))) {
+    const mnemonicWords = process.argv.slice(3);
+    // if (!bip39.validateMnemonic(mnemonicWords.join(" "))) {
     //     console.error("Validation Failed");
     // }
-    const res = await mnemonic2PrivateKey(myMnemonicWords);
-    await getWalletAddress(res[2], parseInt(wc))
+    console.log(mnemonicWords);
+    console.log();
+    const res = await mnemonic2PrivateKey(mnemonicWords);
+
+    WALLET_VERSIONS.forEach(async walletVersion => {
+        await getWalletAddress(res[2], parseInt(wc), walletVersion);
+    });
 })();
